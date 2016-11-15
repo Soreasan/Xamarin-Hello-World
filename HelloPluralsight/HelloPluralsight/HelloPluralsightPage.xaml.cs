3e8802fd4dc9a4cc3e0347ac22b68c99c4d450bc
Kenneth Adair <SOREASAN@GMAIL.COM>
2016-11-15 10:39:28 -0700

    Initial HelloWorld Program
    Initial HelloWorld program created by Xamarin Studio.  Doesn’t compile
    yet and I need to download some stuff.

---
+++ HelloPluralsight/HelloPluralsight/HelloPluralsightPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HelloPluralsight
+{
+	public partial class HelloPluralsightPage : ContentPage
+	{
+		public HelloPluralsightPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
