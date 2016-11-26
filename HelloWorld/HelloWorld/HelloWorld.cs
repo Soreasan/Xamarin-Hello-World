99a8a7b5b3db14048fd980f1ad17560070c1673f
Kenneth Adair <SOREASAN@GMAIL.COM>
2016-11-26 09:25:37 -0700

    Created Simple app
    Created a simple app that says Hello World and then “Goodbye World”
    when you click a button.  Taken from the “Beginning Mobile Development
    with Xamarin” course by Jesse Liberty.

--- HelloWorld/HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -9,22 +9,7 @@ namespace HelloWorld
 		public App()
 		{
 			// The root page of your application
-			var content = new ContentPage
-			{
-				Title = "HelloWorld",
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-
-			MainPage = new NavigationPage(content);
+			MainPage = new MainPage();
 		}
 
 		protected override void OnStart()
---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		public void OnClick(object o, EventArgs e)
+		{
+			message.Text = "Goodbye World!";
+		}
+	}
+}
